a93534211ec34163c834e1280145a5341d84d3b4
fungsilent <funglee162@gmail.com>
2024-06-19 17:50:24 +0800

    update 'Prettier' for support ejs

--- db/models/User.js
+++ db/models/User.js
@@ -5,12 +5,19 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'teacher', 'student'], required: true },
+    role: {
+        type: String,
+        enum: ['admin', 'teacher', 'student'],
+        required: true,
+    },
 })
 
 UserSchema.pre('save', async function (next) {
     if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, process.env.passwordSalt)
+        this.password = await bcrypt.hash(
+            this.password,
+            process.env.passwordSalt
+        )
     }
     next()
 })
